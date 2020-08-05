7b11ee69ee2cdd9362c6f1752583c6f8ddce142c
PresentKim <debe3721@gmail.com>
2020-08-06 04:52:35 +0900

    Refacroting : Use SkinAdapter
    Possible to implement this by simply set the skin adapter....

--- src/blugin/support/SupportCharacterCreator.php
+++ src/blugin/support/SupportCharacterCreator.php
@@ -27,103 +27,30 @@ declare(strict_types=1);
 
 namespace blugin\support;
 
-use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\event\server\DataPacketSendEvent;
-use pocketmine\network\mcpe\JwtUtils;
-use pocketmine\network\mcpe\protocol\LoginPacket;
-use pocketmine\network\mcpe\protocol\PlayerListPacket;
-use pocketmine\network\mcpe\protocol\PlayerSkinPacket;
-use pocketmine\network\mcpe\protocol\types\login\ClientData;
-use pocketmine\network\mcpe\protocol\types\login\ClientDataToSkinDataHelper;
-use pocketmine\network\mcpe\protocol\types\login\JwtChain;
+use pocketmine\entity\Skin;
+use pocketmine\network\mcpe\convert\LegacySkinAdapter;
+use pocketmine\network\mcpe\convert\SkinAdapterSingleton;
 use pocketmine\network\mcpe\protocol\types\SkinData;
 use pocketmine\plugin\PluginBase;
-use pocketmine\uuid\UUID;
 
-class SupportCharacterCreator extends PluginBase implements Listener{
-    /** @var SkinData[] */
-    private $skinData = [];
+class SupportCharacterCreator extends PluginBase{
+    public function onLoad(){
+        SkinAdapterSingleton::set(new class extends LegacySkinAdapter{
+            /** @var SkinData[] */
+            private $personaSkins = [];
 
-    public function onEnable() : void{
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-
-    /**
-     * @priority HIGHEST
-     *
-     * @param DataPacketReceiveEvent $event
-     */
-    public function onDataPacketReceiveEvent(DataPacketReceiveEvent $event) : void{
-        $packet = $event->getPacket();
-        if($packet instanceof LoginPacket){
-            $skinData = $this->getSkinDataFromJwtString($packet->clientDataJwt);
-            $uuid = $this->getUUIDFromJwtChain($packet->chainDataJwt);
-            if($skinData !== null && $uuid !== null){
-                $this->skinData[$uuid->toString()] = $skinData;
-            }
-        }elseif($packet instanceof PlayerSkinPacket){
-            $this->skinData[$event->getOrigin()->getPlayer()->getUniqueId()->toString()] = $packet->skin;
-        }
-    }
-
-    /**
-     * @priority HIGHEST
-     *
-     * @param DataPacketSendEvent $event
-     */
-    public function onDataPacketSendEvent(DataPacketSendEvent $event) : void{
-        foreach($event->getPackets() as $packet){
-            if($packet instanceof PlayerListPacket){
-                foreach($packet->entries as $entry){
-                    $entry->skinData = $this->skinData[$entry->uuid->toString()] ?? $entry->skinData;
+            public function fromSkinData(SkinData $data) : Skin{
+                if($data->isPersona()){
+                    $id = $data->getSkinId();
+                    $this->personaSkins[$id] = $data;
+                    return new Skin($id, str_repeat(random_bytes(3) . "\xff", 2048));
                 }
-            }elseif($packet instanceof PlayerSkinPacket){
-                $packet->skin = $this->skinData[$packet->uuid->toString()] ?? $packet->skin;
+                return parent::fromSkinData($data);
             }
-        }
-    }
 
-    /**
-     * @param JwtChain $chain
-     *
-     * @return UUID|null
-     */
-    public function getUUIDFromJwtChain(JwtChain $chain) : ?UUID{
-        foreach($chain->chain as $k => $jwt){
-            try{
-                [, $payload,] = JwtUtils::parse($jwt);
-                $extraData = $payload["extraData"] ?? null;
-                if(!is_array($extraData))
-                    continue;
-
-                $uuidString = $extraData["identity"] ?? null;
-                if(!is_string($uuidString))
-                    continue;
-
-                return UUID::fromString($uuidString);
-            }catch(\Exception $e){
-                continue;
+            public function toSkinData(Skin $skin) : SkinData{
+                return $this->personaSkins[$skin->getSkinId()] ?? parent::toSkinData($skin);
             }
-        }
-        return null;
-    }
-
-    /**
-     * @param string $jwt
-     *
-     * @return SkinData|null
-     */
-    public function getSkinDataFromJwtString(string $jwt) : ?SkinData{
-        try{
-            [, $payload,] = JwtUtils::parse($jwt);
-            $mapper = new \JsonMapper();
-            $mapper->bEnforceMapType = false;
-            $mapper->bExceptionOnMissingData = true;
-            $mapper->bExceptionOnUndefinedProperty = true;
-            return ClientDataToSkinDataHelper::getInstance()->fromClientData($mapper->map($payload, new ClientData));
-        }catch(\Exception $e){
-            return null;
-        }
+        });
     }
 }
