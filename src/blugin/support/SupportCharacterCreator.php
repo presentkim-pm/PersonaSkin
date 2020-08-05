2400973d6e45864e26006aa4b49b140075003167
PresentKim <debe3721@gmail.com>
2020-08-06 00:46:51 +0900

    Merging JwtParser and main class

--- src/blugin/support/SupportCharacterCreator.php
+++ src/blugin/support/SupportCharacterCreator.php
@@ -30,11 +30,16 @@ namespace blugin\support;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\JwtUtils;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\network\mcpe\protocol\PlayerListPacket;
 use pocketmine\network\mcpe\protocol\PlayerSkinPacket;
+use pocketmine\network\mcpe\protocol\types\login\ClientData;
+use pocketmine\network\mcpe\protocol\types\login\ClientDataToSkinDataHelper;
+use pocketmine\network\mcpe\protocol\types\login\JwtChain;
 use pocketmine\network\mcpe\protocol\types\SkinData;
 use pocketmine\plugin\PluginBase;
+use pocketmine\uuid\UUID;
 
 class SupportCharacterCreator extends PluginBase implements Listener{
     /** @var SkinData[] */
@@ -52,8 +57,8 @@ class SupportCharacterCreator extends PluginBase implements Listener{
     public function onDataPacketReceiveEvent(DataPacketReceiveEvent $event) : void{
         $packet = $event->getPacket();
         if($packet instanceof LoginPacket){
-            $skinData = JwtParser::getSkinDataFromJwtString($packet->clientDataJwt);
-            $uuid = JwtParser::getUUIDFromJwtChain($packet->chainDataJwt);
+            $skinData = $this->getSkinDataFromJwtString($packet->clientDataJwt);
+            $uuid = $this->getUUIDFromJwtChain($packet->chainDataJwt);
             if($skinData !== null && $uuid !== null){
                 $this->skinData[$uuid->toString()] = $skinData;
             }
@@ -76,6 +81,49 @@ class SupportCharacterCreator extends PluginBase implements Listener{
             }elseif($packet instanceof PlayerSkinPacket){
                 $packet->skin = $this->skinData[$packet->uuid->toString()] ?? $packet->skin;
             }
+        }
+    }
+
+    /**
+     * @param JwtChain $chain
+     *
+     * @return UUID|null
+     */
+    public function getUUIDFromJwtChain(JwtChain $chain) : ?UUID{
+        foreach($chain->chain as $k => $jwt){
+            try{
+                [, $payload,] = JwtUtils::parse($jwt);
+                $extraData = $payload["extraData"] ?? null;
+                if(!is_array($extraData))
+                    continue;
+
+                $uuidString = $extraData["identity"] ?? null;
+                if(!is_string($uuidString))
+                    continue;
+
+                return UUID::fromString($uuidString);
+            }catch(\Exception $e){
+                continue;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param string $jwt
+     *
+     * @return SkinData|null
+     */
+    public function getSkinDataFromJwtString(string $jwt) : ?SkinData{
+        try{
+            [, $payload,] = JwtUtils::parse($jwt);
+            $mapper = new \JsonMapper();
+            $mapper->bEnforceMapType = false;
+            $mapper->bExceptionOnMissingData = true;
+            $mapper->bExceptionOnUndefinedProperty = true;
+            return ClientDataToSkinDataHelper::getInstance()->fromClientData($mapper->map($payload, new ClientData));
+        }catch(\Exception $e){
+            return null;
         }
     }
 }
