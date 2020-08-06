48ab0803ab91cddd1c4ebe2c28a2e2d3d2f95dae
PresentKim <debe3721@gmail.com>
2020-08-06 13:01:23 +0900

    Separate anonymous classes to ParsonaSkinAdapter

--- src/blugin/personaskin/ParsonaSkinAdapter.php
+++ src/blugin/personaskin/ParsonaSkinAdapter.php
@@ -27,11 +27,24 @@ declare(strict_types=1);
 
 namespace blugin\personaskin;
 
-use pocketmine\network\mcpe\convert\SkinAdapterSingleton;
-use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Skin;
+use pocketmine\network\mcpe\convert\LegacySkinAdapter;
+use pocketmine\network\mcpe\protocol\types\SkinData;
 
-class PersonaSkin extends PluginBase{
-    public function onLoad(){
-        SkinAdapterSingleton::set(new ParsonaSkinAdapter);
+class ParsonaSkinAdapter extends LegacySkinAdapter{
+    /** @var SkinData[] */
+    private $personaSkins = [];
+
+    public function fromSkinData(SkinData $data) : Skin{
+        if($data->isPersona()){
+            $id = $data->getSkinId();
+            $this->personaSkins[$id] = $data;
+            return new Skin($id, str_repeat(random_bytes(3) . "\xff", 2048));
+        }
+        return parent::fromSkinData($data);
+    }
+
+    public function toSkinData(Skin $skin) : SkinData{
+        return $this->personaSkins[$skin->getSkinId()] ?? parent::toSkinData($skin);
     }
 }
