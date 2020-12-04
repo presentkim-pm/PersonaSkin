479e4091be42941c23e0e58f72a70ff8580f91fb
PresentKim <debe3721@gmail.com>
2020-12-04 22:22:40 +0900

    refactor : Implement the PersonaSkin class (extends Skin)
    So that it can be accessed from external plugins

--- src/kim/present/personaskin/ParsonaSkinAdapter.php
+++ src/kim/present/personaskin/ParsonaSkinAdapter.php
@@ -31,19 +31,14 @@ use pocketmine\network\mcpe\protocol\types\LegacySkinAdapter;
 use pocketmine\network\mcpe\protocol\types\SkinData;
 
 class ParsonaSkinAdapter extends LegacySkinAdapter{
-    /** @var SkinData[] */
-    private $personaSkins = [];
-
     public function fromSkinData(SkinData $data) : Skin{
         if($data->isPersona()){
-            $id = $data->getSkinId();
-            $this->personaSkins[$id] = $data;
-            return new Skin($id, str_repeat(random_bytes(3) . "\xff", 2048));
+            return new PersoanSkin($data);
         }
         return parent::fromSkinData($data);
     }
 
     public function toSkinData(Skin $skin) : SkinData{
-        return $this->personaSkins[$skin->getSkinId()] ?? parent::toSkinData($skin);
+        return $skin instanceof PersoanSkin ? $skin->getPersonaSkinData() : parent::toSkinData($skin);
     }
 }
